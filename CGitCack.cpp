77bc670f640c3c23155a4f2bd2df19638b137008
dws14159 <davethesax@gmail.com>
2017-09-05 10:42:57 +0100

    Attempt to create filename containing question mark

--- CGitCack.cpp
+++ CGitCack.cpp
@@ -3,9 +3,27 @@
 
 #include "stdafx.h"
 
+#define TEST test1
+
+// Can we create a filename with a question mark in it?  Not with this approach, we consistently get err 22.
+void test1()
+{
+    char *filename = "D:\\Hello\077World.txt";
+    FILE *fp;
+    int err = fopen_s(&fp, filename, "w");
+    if (err)
+    {
+        printf("err=%d\n", err);
+    }
+    else
+    {
+        fprintf(fp, "Greetings earthling!\n");
+        fclose(fp);
+    }
+}
 
 int main()
 {
+    TEST();
     return 0;
 }
-
