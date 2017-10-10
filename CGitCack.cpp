004cfd2fd4bacd65491bd61ef6cd77ec48198674
dws14159 <davethesax@gmail.com>
2017-10-10 15:24:20 +0100

    converting double to hex and back

--- CGitCack.cpp
+++ CGitCack.cpp
@@ -3,7 +3,37 @@
 
 #include "stdafx.h"
 
-#define TEST test1
+#define TEST test2
+
+// converting double to hex and back https://stackoverflow.com/q/46668785/5937167
+void test2()
+{
+    printf("sizeof double=%d\n", sizeof(double)); // 8
+
+    union
+    {
+        unsigned char bytes[8];
+        double dbl;
+    } urep;
+
+    urep.dbl = -0.15625;
+    printf("Hex representation of %f is ", urep.dbl);
+    for (int i = 0; i < 8; i++)
+        printf("%02x ", urep.bytes[i]);
+    printf("\n");
+    // Hex representation of 27.395000 is 85 eb 51 b8 1e 65 3b 40
+
+    memset(&urep, 0, sizeof(urep));
+    urep.bytes[0] = 0x85;
+    urep.bytes[1] = 0xeb;
+    urep.bytes[2] = 0x51;
+    urep.bytes[3] = 0xb8;
+    urep.bytes[4] = 0x1e;
+    urep.bytes[5] = 0x65;
+    urep.bytes[6] = 0x3b;
+    urep.bytes[7] = 0x40;
+    printf("String to double result: %f\n", urep.dbl);
+}
 
 // Can we create a filename with a question mark in it?  Not with this approach, we consistently get err 22.
 void test1()
